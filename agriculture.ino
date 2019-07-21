df3884bde8d78cf76ae11679fa3d250e680b1873
Mert Akengin <mert.akengin@iyzico.com>
2019-07-21 17:23:32 +0300

    test version
    - add listener
    - update proto
    - add master mode

--- agriculture.ino
+++ agriculture.ino
@@ -36,26 +36,58 @@
 #define PIN_SPI3 27
 #define PIN_SPI4 18
 
-typedef struct __attribute__((packed)) {
-	size_t size;
-	void *data;
-	short rssi;
-	float snr;
-} lora_t;
+#define PIN_MASTER 0 // masterswitch
 
+#define NET_IP_SRC WiFi.localIP()
+#define NET_IP_DST IPAddress(224, 0, 0, 1)
+#define NET_PORT   4321
+
+#define SLEEP_TTL 10E6 // uS
+#define MPRINTF Serial.printf
+
+typedef struct {
+	union {
+		struct {
+			bool parsed;
+			size_t size;
+			short rssi;
+			float snr;
+		} data;
+		unsigned char pad[16];
+	} meta;
+	union {
+		data_package_t pkg;
+		void *ptr;
+	} data;
+} __attribute__((packed)) lora_pkg_t;
+
+static WiFiUDP udp;
+static bool isr_state;
+static lora_pkg_t lora_last_pkg;
 const SPIClass hspi(HSPI);
 const unsigned long device_identifier = (uint32_t) (ESP.getEfuseMac() >> 16);
 static unsigned long packet_counter = 0;
+RTC_DATA_ATTR unsigned long boot_count = 0UL;
 
-void onReceive(int size) {
-	debug("lora", "received: %d", size);
-	if(size != sizeof(data_package_t)) {
+void lora_receive_handler(int size) {
+	lora_pkg_t pkg = {
+		.meta = {
+			.data = {
+				.parsed = false,
+				.size   = (unsigned) size,
+				.rssi   = (short) LoRa.packetRssi(),
+				.snr    = (float) LoRa.packetSnr(),
+			},
+		},
+	};
+	debug("lora", "received: %d (rssi:%d snr:%f)", size, pkg.meta.data.rssi, pkg.meta.data.snr);
+	if(size != sizeof(pkg.data.pkg)) {
+		debug("lora", "size mismatch: actual:%d, expected:%d", size, sizeof(pkg.data.pkg));
 		return;
 	}
-	data_package_t pkg;
-	LoRa.readBytes((char*) &pkg, sizeof(pkg));
-	debug("lora", "rssi: %d, snr: %f", LoRa.packetRssi(), LoRa.packetSnr());
-	__hexdump(&pkg, sizeof(pkg));
+	LoRa.readBytes((char*) &(pkg.data.pkg), sizeof(pkg.data.pkg));
+	//__hexdump(&pkg.data.pkg, sizeof(pkg.data.pkg));
+	lora_last_pkg = pkg; // copy back
 	return;
 }
 
@@ -66,7 +98,7 @@ void debug(const char *mod, const char *fmt, ...) {
 	va_start(args, fmt);
 	char large[256];
 	vsprintf(large, buffer, args);
-	ets_printf(large);
+	MPRINTF(large);
 	//vprintf(buffer, args);
 	va_end(args);
 	return;
@@ -101,53 +133,35 @@ void init(unsigned long freq) {
 }
 */
 
-void setup(void) {
-	delay(999);
-	Serial.begin(115200);
-	Serial.printf("# %16llx\r\n", device_identifier);
-
-	pinMode(LORA_DIO, INPUT);
-	pinMode(LORA_SS,     OUTPUT);
-	pinMode(PWR_LORA,    OUTPUT);
-	pinMode(PWR_SENSOR1, OUTPUT);
-	pinMode(PWR_SENSOR2, OUTPUT);
-	pinMode(PIN_SENSOR0, INPUT);
-	pinMode(PIN_SENSOR1, INPUT);
-	pinMode(PIN_SENSOR2, INPUT);
-	pinMode(PIN_SENSOR3, INPUT);
-	digitalWrite(PWR_LORA,    HIGH);
-	digitalWrite(PWR_SENSOR1, HIGH);
-	digitalWrite(PWR_SENSOR2, HIGH);
-
-	//hspi.begin();
-	SPI.begin(PIN_SPI1, PIN_SPI2, PIN_SPI3, PIN_SPI4);
-	LoRa.crc();
-	//LoRa.setSPI(hspi);
-	//LoRa.setSPIFrequency(8E6);
-	LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO);
-	while(!LoRa.begin(LORA_FREQ)) {
-		debug("lora", "trying...");
-		delay(999);
+void handle_wake(esp_sleep_wakeup_cause_t cause) {
+	boot_count += 1;
+	switch(cause) {
+		case ESP_SLEEP_WAKEUP_EXT0:
+			break;
+		case ESP_SLEEP_WAKEUP_EXT1:
+			break;
+		case ESP_SLEEP_WAKEUP_TIMER:
+			break;
+		case ESP_SLEEP_WAKEUP_TOUCHPAD:
+			break;
+		case ESP_SLEEP_WAKEUP_ULP:
+			break;
+		default:
+			break;
 	}
-	LoRa.setSyncWord(LORA_SYNC);
-	//LoRa.dumpRegisters(Serial);
-	LoRa.onReceive(onReceive);
-	LoRa.receive();
-	return yield();
-}
-
-void __hexdump(void *data, size_t size) {
-	static const int width = 8;
-	for(int i=0; i<size; i++) {
-		if(!(i%width)) Serial.printf("\r\n%8x |", i);
-		Serial.printf(" % 2hhx", *(char*)(data+i));
-		continue;
-	}
-	Serial.printf("\r\n");
 	return;
 }
 
-void loop(void) {
+void mode_master() {
+	char ssid[64];
+	sprintf(ssid, "agr-%08x", device_identifier);
+	WiFi.disconnect(true);
+	WiFi.softAP(ssid);
+	udp.begin(NET_IP_SRC, NET_PORT);
+	return yield();
+}
+
+void mode_slave() {
 	data_package_t pkg = make_data(
 		++packet_counter, device_identifier,
 		0xFFFFFFFFUL, 0xFFFFU,
@@ -160,5 +174,85 @@ void loop(void) {
 	debug("lora", "sending: %d", sizeof(pkg));
 	__hexdump(&pkg, sizeof(pkg));
 	lora_send(&pkg, sizeof(pkg));
-	return delay(9999);
+	debug("core", "sleeping... (%d)", (SLEEP_TTL/1E3));
+	esp_sleep_enable_ext0_wakeup(GPIO_NUM_0, LOW);
+	esp_sleep_enable_timer_wakeup(SLEEP_TTL);
+	esp_deep_sleep_start();
+	return yield();
+}
+
+void IRAM_ATTR isr(void) {
+	MPRINTF("isr handled");
+	while(isr_state); // hacky restart
+	isr_state = 0x1;
+	return;
+}
+
+void setup(void) {
+	delay(999);
+	Serial.begin(115200);
+	MPRINTF("# %16llx\r\n", device_identifier);
+	handle_wake(esp_sleep_get_wakeup_cause());
+	pinMode(PIN_MASTER,  INPUT_PULLUP);
+	pinMode(LORA_DIO,    INPUT);
+	pinMode(LORA_SS,     OUTPUT);
+	pinMode(PWR_LORA,    OUTPUT);
+	pinMode(PWR_SENSOR1, OUTPUT);
+	pinMode(PWR_SENSOR2, OUTPUT);
+	pinMode(PIN_SENSOR0, INPUT);
+	pinMode(PIN_SENSOR1, INPUT);
+	pinMode(PIN_SENSOR2, INPUT);
+	pinMode(PIN_SENSOR3, INPUT);
+	digitalWrite(PWR_LORA,    HIGH);
+	digitalWrite(PWR_SENSOR1, HIGH);
+	digitalWrite(PWR_SENSOR2, HIGH);
+	attachInterrupt(PIN_MASTER, isr, RISING);
+	//hspi.begin();
+	SPI.begin(PIN_SPI1, PIN_SPI2, PIN_SPI3, PIN_SPI4);
+	LoRa.crc();
+	//LoRa.setSPI(hspi);
+	//LoRa.setSPIFrequency(8E6);
+	LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO);
+	while(!LoRa.begin(LORA_FREQ)) {
+		debug("lora", "trying...");
+		delay(999);
+	}
+	LoRa.setSyncWord(LORA_SYNC);
+	//LoRa.dumpRegisters(Serial);
+	LoRa.onReceive(lora_receive_handler);
+	LoRa.receive();
+	for(int i=0; i<10; i++) {
+		debug("core", "waiting for masterswitch");
+		if(isr_state || !digitalRead(PIN_MASTER)) {
+			debug("core", "masterswitch found!");
+			return mode_master();
+		}
+		delay(333);
+		continue;
+	}
+	// slave mode begins here
+	debug("core", "thats slavery with extra steps");
+	return mode_slave();
+}
+
+void __hexdump(void *data, size_t size) {
+	static const int width = 8;
+	for(int i=0; i<size; i++) {
+		if(!(i%width)) MPRINTF("\r\n%8x |", i);
+		MPRINTF(" % 2hhx", *(char*)(data+i));
+		continue;
+	}
+	MPRINTF("\r\n");
+	return;
+}
+
+void loop(void) {
+	if(lora_last_pkg.meta.data.parsed) {
+		return yield();
+	}
+	udp.beginPacket(NET_IP_DST, NET_PORT);
+	udp.write((unsigned char*) &lora_last_pkg, sizeof(lora_last_pkg));
+	udp.endPacket();
+	lora_last_pkg.meta.data.parsed = true;
+	return yield();
 }
---
+++ listen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os, sys, time, socket
+
+ADDR = "224.0.0.1"
+PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 4321
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, True)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+sock.bind((ADDR, PORT))
+
+print("listening on: {}".format(PORT))
+
+hexdump = lambda data, width=8, fmt="{hex:2X}": " ".join([
+		("\n{index:4X} | " + fmt).format(index=i, hex=(b))
+		if i%width is 0
+		else fmt.format(index=i, hex=(b))
+		for i, b in enumerate(data)
+	])
+
+while True:
+	(data, addr) = sock.recvfrom(256)
+	print("{ip}:{port}: #{length}: {data}".format(
+		ip=addr[0],
+		port=addr[1],
+		data=hexdump(data),
+		length=len(data),
+	))
+	continue
