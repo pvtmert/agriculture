e77785bb27a3d0a7fbda125b7059f30c703edc91
Mert Akengin <mert.akengin@iyzico.com>
2019-07-31 00:34:16 +0300

    update listener, add packet generator

---
+++ genpkg.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os, sys, time, socket, struct, json, random, hashlib
+
+from listen import lora, meta, header, payload, hexdump, getTime
+from listen import lora_meta, lora_header, lora_payload, config
+
+ADDR = "224.0.0.1"
+PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 4321
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, True)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+
+counter = 1
+
+while True:
+	data = bytearray(96)
+	lora_meta.pack_into(data, 0x00, *meta(
+		parse=0,
+		size=80,
+		rssi=random.randint(-160, -30),
+		snr=0.0,
+	))
+	lora_header.pack_into(data, 0x10, *header(
+		magic=0x12345678,
+		ver=0xABCD,
+		dst=0x30e543a4,
+		id=counter,
+		ts=int(time.time()),
+		cs=0xFFFFFFFF,
+		src=0xFFFFFFFF,
+		size=233,
+		ttl=0x80,
+		net=0x20,
+		flags=0xFF,
+	))
+	lora_payload.pack_into(data, 0x40, *payload(
+		ver=0xBEEF,
+		s30=random.randint(10, 30),
+		s60=random.randint(30, 60),
+		s90=random.randint(60, 90),
+		temp=random.randint(0, 100),
+		ec=random.randint(100, 200),
+		vp=random.randint(200, 300),
+	))
+	print("sending: ", hex(counter), hexdump(data))
+	sock.sendto(data, (ADDR, PORT))
+	time.sleep(5.0)
+	counter += 1
+	continue
--- listen.py
+++ listen.py
@@ -2,16 +2,7 @@
 
 import os, sys, time, socket, struct, json
 
-#ADDR = "224.0.0.1"
-PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 4321
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, True)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-sock.bind(("", PORT))
-
-print("listening on: {}".format(PORT))
+from collections import namedtuple
 
 hexdump = lambda data, width=8, fmt="{hex:2X}": " ".join([
 		("\n{index:4X} | " + fmt).format(index=i, hex=(b))
@@ -20,7 +11,44 @@ hexdump = lambda data, width=8, fmt="{hex:2X}": " ".join([
 		for i, b in enumerate(data)
 	])
 
+lora = namedtuple("lora", [
+	"meta",
+	"header",
+	"payload",
+])
+meta = namedtuple("meta", [
+	"parse",
+	"size",
+	"rssi",
+	"snr",
+])
+header = namedtuple("header", [
+	"magic",
+	"ver",
+	"dst",
+	"id",
+	"ts",
+	"cs",
+	"src",
+	"size",
+	"ttl",
+	"net",
+	"flags",
+])
+payload = namedtuple("payload", [
+	"ver",
+	"s30",
+	"s60",
+	"s90",
+	"temp",
+	"ec",
+	"vp",
+])
+
 config = struct.Struct("<5L")
+lora_meta = struct.Struct("<LLhf")
+lora_header = struct.Struct("<LH5L4B")
+lora_payload = struct.Struct("<7H")
 
 def getTime(filename: str, ident: str, default=int(15e6), devices={}):
 	if os.path.exists(filename):
@@ -30,26 +58,75 @@ def getTime(filename: str, ident: str, default=int(15e6), devices={}):
 		return default
 	return int(devices[ident])
 
-while True:
-	(data, addr) = sock.recvfrom(256)
-	print("{ip}:{port}: #{length}: {data}".format(
-		ip=addr[0],
-		port=addr[1],
-		data=hexdump(data),
-		length=len(data),
-	))
-	lora_addr = struct.unpack_from("<L", data, 0x28)
-	lora_data = struct.unpack_from("<6H", data, 0x42)
-	#print([ hex(i) for i in lora_addr ])
-	print("sensors:", lora_data)
-	mbuffer = bytearray(32)
-	config.pack_into(mbuffer, 0x0,
-		lora_addr[0], # TARGET ADDR
-		0x12345678,  # save
-		0x87654321,  # mode
-		0xABC00DEF,  # mesh
-		getTime("devices.json", "{:08x}".format(*lora_addr)), # sleep
-	)
-	print("sending: ", hex(lora_addr[0]), hexdump(mbuffer))
-	sock.sendto(mbuffer, addr)
-	continue
+"""
+192.168.4.1:4321: #96:
+   0 |  0  0  0  0 50  0  0  0
+   8 | B5 FF  0  0  0  0 24 41 <- ends lora part
+  10 | 78 56 34 12 CD AB  0  0
+  18 | FF FF FF FF  7  0  0  0
+  20 | 53  9  0  0 22 B4 14 C2
+  28 | A4 43 E9 44 20 FF 99  0
+  30 |  0  0  0  0  0  0  0  0
+  38 |  0  0  0  0  0  0  0  0
+  40 | BE EF FF  F  0  0  0  0 <- starts data "beef"
+  48 | FF  F FF  F  0  0  0  0
+  50 |  0  0  0  0  0  0  0  0
+  58 |  0  0  0  0  0  0  0  0
+"""
+
+"""
+(
+	lora_magic,
+	lora_ver,
+	lora_dst,
+	lora_id,
+	lora_ts,
+	lora_cs,
+	lora_src,
+	lora_size,
+	lora_ttl,
+	lora_net,
+	lora_flags,
+) = lora_header.unpack_from(data, 0x10)
+"""
+
+def main():
+	#ADDR = "224.0.0.1"
+	PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 4321
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, True)
+	sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+	sock.bind(("", PORT))
+	print("listening on: {}".format(PORT))
+	while True:
+		(data, addr) = sock.recvfrom(256)
+		print("{ip}:{port}: #{length}: {data}".format(
+			ip=addr[0],
+			port=addr[1],
+			data=hexdump(data),
+			length=len(data),
+		))
+		#lora_data = struct.unpack_from("<6H", data, 0x42)
+		print(
+			"\nmeta:",          meta(*lora_meta.unpack_from(data, 0x00)),
+			"\nheaders:",   header(*lora_header.unpack_from(data, 0x10)),
+			"\nsensors:", payload(*lora_payload.unpack_from(data, 0x40)),
+		)
+		mbuffer = bytearray(32)
+		lora_addr = struct.unpack_from("<L", data, 0x28)[0]
+		config.pack_into(mbuffer, 0x0,
+			lora_addr, # TARGET ADDR
+			0x12345678,  # save
+			0x87654321,  # mode
+			0xABC00DEF,  # mesh
+			getTime("devices.json", "{:08x}".format(lora_addr)), # sleep
+		)
+		print("sending: ", hex(lora_addr), hexdump(mbuffer))
+		sock.sendto(mbuffer, addr)
+		continue
+	return
+
+
+if __name__ == '__main__':
+	main()
